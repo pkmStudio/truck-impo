0bfc0d78ba3b99ed365e75b700843b533fa1d613
Edvard <pmkstudio77@gmail.com>
2025-05-10 21:38:49 +0300

    Установка laravel UI и чистка от ненужных файлов.

---
+++ app/Http/Controllers/AdminAuthController.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class AdminAuthController extends Controller
+{
+    public function showLoginForm()
+    {
+        return view('admin.auth.login');
+    }
+
+    public function login(Request $request)
+    {
+        $credentials = $request->validate([
+            'email' => 'required|email',
+            'password' => 'required',
+        ]);
+
+        if (!Auth::attempt($credentials)) {
+            return back()->withErrors(['error' => 'Неверные данные']);
+        }
+
+        $user = Auth::user();
+
+        // Проверяем, является ли пользователь админом
+        if (!$user->is_admin) {
+            Auth::logout();
+            return back()->withErrors(['error' => 'Доступ только для админов']);
+        }
+
+        return redirect()->route('admin.index'); // Перенаправляем в админку
+    }
+
+    public function logout()
+    {
+        Auth::logout();
+        return redirect('/admin/login');
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,8 @@
 use App\Http\Controllers\Admin\BrandController;
 use App\Http\Controllers\Admin\CategoryController;
 use App\Http\Controllers\Admin\ProductController;
+use App\Http\Controllers\AdminAuthController;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/admin', function () {
@@ -14,3 +16,10 @@ Route::group(['prefix' => 'admin'], function () {
     Route::resource('/categories', CategoryController::class)->names('admin.categories');
     Route::resource('/products', ProductController::class)->names('admin.products');
 });
+
+Route::get('/admin/login', [AdminAuthController::class, 'showLoginForm'])->name('admin.login');
+Route::post('/admin/login', [AdminAuthController::class, 'login']);
+Route::post('/admin/logout', [AdminAuthController::class, 'logout'])->name('admin.logout');
+
+
+Route::get('/home', [App\Http\Controllers\AdminAuthController::class, 'index'])->name('home');
