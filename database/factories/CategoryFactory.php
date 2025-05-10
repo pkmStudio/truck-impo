e33a70f6f3389868afcf3fdf4d09da47e99a9838
Edvard <pmkstudio77@gmail.com>
2025-05-10 13:35:04 +0300

    Изменил формат столбца контент на JSON

--- database/factories/CategoryFactory.php
+++ database/factories/CategoryFactory.php
@@ -19,7 +19,11 @@ class CategoryFactory extends Factory
         return [
             'title' => fake()->unique()->word(),
             'description' => fake()->text,
-            'content' => fake()->json,
+            'content' => '{
+    "first-section": "<header><h1>Категория: Запчасти для BMW</h1></header>",
+    "second-section": "<section><p>Здесь вы найдете все запчасти для автомобилей BMW, включая тормоза, диски и масла.</p></section>",
+    "third-section": "<footer><p>© 2025 Все права защищены. Свяжитесь с нами для консультации.</p></footer>"
+}',
             'image_path' => fake()->imageUrl,
             'slug' => fake()->unique()->slug,
         ];
